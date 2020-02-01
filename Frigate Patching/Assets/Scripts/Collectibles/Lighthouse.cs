38bf671f89c07b0e6cc5d14e7af4eeb43256ef4a
Alexandru Grosu <alex.gm98@yahoo.com>
2020-02-01 20:32:36 +0200

    Added Lighthouse

---
+++ Frigate Patching/Assets/Scripts/Collectibles/Lighthouse.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighthouse : MonoBehaviour, ICollectible
+{
+    public float value = 25;
+    private Player player;
+    private Animator animator;
+
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player = collision.gameObject.GetComponent<Player>();
+            Collect();
+        }
+    }
+
+    public void Collect()
+    {
+        animator.SetBool("Used", true);
+        player.Heal(value);
+        GameEvents.PlayerUpdateHealth(player);
+    }
+}
