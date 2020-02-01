a7c8b9a7613085a59fbce738a1ef34cb847ef3cd
Metalqgata <slayer_hmr@abv.bg>
2020-02-01 18:31:20 +0200

    Menu functionality
    Menu functionality with a few test script

---
+++ Frigate Patching/Assets/Scripts/Menu/MenuActions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour
+{
+    public string scene; 
+    //To quit the game
+   public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    //To load the game
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
